b650dcdd42f58807766d31222c0bb106f53d6884
Isaac J <duckhunterjr@gmail.com>
2023-02-06 21:18:53 -0800

    add initial form

---
+++ test-isaac.php
@@ -0,0 +1,31 @@
+<?php
+//inital work
+
+
+
+
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+    <div class="card">
+        <h2>Item Name</h2>
+        <p>Description of item</p>
+    </div>
+
+    </form>
+</body>
+</html>
