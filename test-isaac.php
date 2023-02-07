5713b4f3fbbadfcacdc24e9035cdbdaf9ab7b53d
Isaac J <duckhunterjr@gmail.com>
2023-02-07 15:51:30 -0800

    add menu item for loop

--- test-isaac.php
+++ test-isaac.php
@@ -6,6 +6,7 @@ class Item
     public $name = '';
     public $description = '';
     public $price = 0;
+    public $addon = array();
 
     public function __construct($ID, $name, $description, $price)
     {
@@ -15,12 +16,31 @@ class Item
         $this->price = $price;
     }
 
+    public function addon($addon){
+        $this->addon[] = $addon;
+    }
+
 }
+
+//save new instance to variable
+$items = new Item(0,'Spicy Ahi Bowl','Spicy Ahi Tuna, Cucumber, Sweet Onion, Scallion, House Shoyu
+Spicy Aioli, Masago ,Pickled Ginger',12.95);
+// $item->addon('Tuna');
+// $item->addon('Rice');
+// $item->addon('Onions');
+$menuItems[]= $items;
+
+$items = new Item(1,'Hawaiian Classic Bowl','Ahi Tuna, Sweet Onion Hijiki (Seaweed), Scallion, House Shoyu Seaweed Salad, Sesame Seeds',13.95);
+$menuItems[]= $items;
+
+$items = new Item(2,'Pike Place Bowl','Salmon, Sweet Onion, Cucumber, Sesame Seeds, House Shoyu, Edamame, Crunchy Onion',13.95);
+$menuItems[]= $items;
 
 //Grab form
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     echo '<pre>';
     echo var_dump($_POST);
+    echo var_dump($menuItems);
     echo '</pre>';
 }
 ?>
@@ -37,18 +57,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
     <div class="card-container">
-        <div class="card">
-            <h2>Item Name</h2>
-            <p>Description of item</p>
+        <?php foreach( $menuItems as $item) {
+        echo '<div class="card">
+            <h2>'. $item->name .'</h2>
+            <p>'. $item->description .'</p>
             <label>Quantity</label>
-            <select name="quantity">
+            <select name="quantity'.$item->ID.'">
                 <option value="">0</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
-
+                <option value="4">4</option>
+                <option value="5">5</option>
             </select>     
-        </div>
+        </div>';
+        }
+        ?>
     </div>
     <button type="submit">Submit</button>
     </form>
